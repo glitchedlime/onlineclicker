d20468ea6ba0903005adccf11935efe81348f718
glitchedlime <gabcosamuel8@gmail.com>
2026-08-13 15:29:02 +0200

    update v1.2

--- update_server_git.bat
+++ update_server_git.bat
@@ -1,6 +1,6 @@
 @echo off
 git fetch origin
-git reset --hard origin
+git reset --hard origin/HEAD
 echo.
 echo Your server has been updated. Press ENTER to close this window.
 pause > nul
--- update_server_git.sh
+++ update_server_git.sh
@@ -1,3 +1,3 @@
 git fetch origin
-git reset --hard origin
+git reset --hard origin/HEAD
 read -p "Your server has been updated. Press ENTER to close this window."
